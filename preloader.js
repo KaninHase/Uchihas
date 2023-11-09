e188c9b8b128536b937b1e4a7b0fa83bee615584
KaninUchiha <97570748+KaninHase@users.noreply.github.com>
2023-11-09 14:22:11 -0600

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+
+	setTimeout(function(){
+		$('html').addClass('loaded');
+		$('body1').css('color','#222222');
+	}, 1000);
+
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+$(window).on('load',function(){
+	setTimeout(function(){ // allowing 3 secs to fade out loader
+	$('.sharingan-loader').fadeOut('slow');
+	},3500);
+});
